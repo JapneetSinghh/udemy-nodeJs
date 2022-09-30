0f715e4ee4883472f607ddc8780dc404a17c3c96
Japneet Singh Baluja <96378977+JapneetSinghh@users.noreply.github.com>
2022-09-30 07:24:53 +0000

    Delete routes directory

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const path=require('path');
-const rootDir= require('../util/path');
-routes.get('/users',(req,res,next)=>{
-res.sendFile(path.join(rootDir,'views','users.html'));
-});
-
-module.exports=routes;
